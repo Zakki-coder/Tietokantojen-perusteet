66f337eaad6dee1b22bc763444df7fb4aebfe56a
jniemine <nieminen.jakke@gmail.com>
2023-04-01 18:40:50 +0300

    Made few functions

--- 1./bikes.py
+++ 1./bikes.py
@@ -1,4 +1,5 @@
 import sqlite3
+import math
 
 # Connect to the database
 db = sqlite3.connect('bikes.db')
@@ -6,9 +7,41 @@ db.isolation_level = None
 
 # Get a cursor to the database
 
+# Print table
+def print_table(table):
+	for row in table:
+		print(row)
+
 # Define function to get the distance of a user
 def distance_of_user(user):
 	# Get user id
 	row = db.execute("SELECT id FROM Users WHERE name=?", [user]) #.fetchone()
-	userName = row.fetchone()
-	print(userName[0])
+	user_id = row.fetchone()
+	# Get sum of distances
+	sum_of_distances = db.execute("SELECT SUM(distance) FROM Trips WHERE user_id=?", [user_id[0]]).fetchall()
+	#print(f"Sum of distances: {sum_of_distances[0][0]}")
+	return(sum_of_distances[0][0])
+
+# Function that returns the average speed of user
+def speed_of_user(user):
+	# Calculate distance / time
+	user_id = db.execute("SELECT id FROM Users WHERE name=?", [user]).fetchone()
+	trip_times = db.execute("SELECT SUM(duration) FROM Trips WHERE user_id=?", [user_id[0]]).fetchone()
+	# Convert minutes to hours
+	trip_times_in_hours = trip_times[0] / 60
+	# Divide with 1000 to convert meters into kilometers
+	return(round(distance_of_user(user) / trip_times_in_hours / 1000, 2))
+
+# Function that tells for every city the total drive time
+def duration_in_each_city(day):
+	result = []
+	cities = db.execute("SELECT * FROM Cities").fetchall()
+	for city in cities:
+		result.append((city[1], db.execute("SELECT SUM(duration) FROM Bikes JOIN Trips ON Bikes.id = Trips.bike_id WHERE city_id=? AND day=?", (city[0], day)).fetchone()[0]))
+	return result
+
+# Function that tells number of users in given city
+def users_in_city(city):
+	city_id = db.execute("SELECT id FROM Cities WHERE Cities.name=?", [city]).fetchone()
+	number_of_users = db.execute("SELECT COUNT(DISTINCT user_id) FROM Bikes JOIN Trips ON Bikes.id = Trips.bike_id WHERE Bikes.city_id =?", [city_id[0]]).fetchone()
+	return number_of_users[0]
--- 1./main.py
+++ 1./main.py
@@ -1,8 +1,19 @@
 import bikes
 
+distance_of_user = bikes.distance_of_user("user123")
 print("Test 1:", bikes.distance_of_user("user123"))
-# print("Test 2:", bikes.speed_of_user("user123"))
-# print("Test 3:", bikes.duration_in_each_city("2021-06-01"))
-# print("Test 4:", bikes.users_in_city("city5"))
+assert distance_of_user == 130160, "Distance of user should be 130160"
+
+speed_of_user = bikes.speed_of_user("user123")
+print("Test 2:", speed_of_user)
+assert speed_of_user == 17.35, "Speed of user should be 17.35"
+
+duration_in_each_city = bikes.duration_in_each_city("2021-06-01")
+print("Test 3:", duration_in_each_city)
+assert duration_in_each_city == [('city1', 58655), ('city2', 60947), ('city3', 59470), ('city4', 57488), ('city5', 59829), ('city6', 59925), ('city7', 57403), ('city8', 59354), ('city9', 59749), ('city10', 59296)], "Differences in arrays"
+
+users_in_city = bikes.users_in_city("city5")
+print("Test 4:", users_in_city)
+assert users_in_city == 43102, "Users in city should be 43102"
 # print("Test 5:", bikes.trips_on_each_day("city5"))
 # print("Test 6:", bikes.most_popular_start("city5"))
